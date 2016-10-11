9a55e3afe5fd03413ff7c62841fbdcb7ced80973
mwongka <mikekawong@gmail.com>
2016-10-11 11:27:46 -0700

    fixing bug stashing for later

--- client/app/home/inputBar/inputBarPresentational.js
+++ client/app/home/inputBar/inputBarPresentational.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import FriendDropDownContainer from './friendDropDown/friendDropDownContainer';
 
-const friends = [{name: 'Friends', id: '0'},{name: 'Damien', id: '1'}, {name: 'Wendy', id: '2'}, {name: 'Jordan', id: '3'}];
-
 const InputBarPresentational = (props) => {
   console.log(props.friends, 'which one is this');
   return (
